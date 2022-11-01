51d1f5ff3176d83a05c75b897a4dca9bd3796f4b
kirbyfridge <114848971+kirbyfridge@users.noreply.github.com>
2022-11-01 18:04:49 -0400

    adds list items but still needs to write them

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,5 +2,18 @@ var shoppingFormEl = $('#shopping-form');
 var shoppingListEl = $('#shopping-list');
 
 // TODO: Create a function to handle the form submission event that captures the form's `<input>` value and prints it to the `shoppingListEl` as a `<li>`
+function handleFormSubmit(event) {
+    event.preventDefault();
+
+    var shoppingItem = $('<li>');
+
+    shoppingItem.text = $('#shopping-input').val();
+
+    shoppingListEl.append(shoppingItem);
+
+    $('input[type="text"]').val('');
+}
 
 // TODO: Add an event listener to the `shoppingFormEl` to handle submission
+shoppingFormEl.on('submit', handleFormSubmit)
+
